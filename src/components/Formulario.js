d083e45c8becadfcd2574ca50fd99c04f6c18206
JoseIgnacioTapia <josignaciot@gmail.com>
2021-07-20 20:57:59 -0300

    Building the Form Component

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,34 @@
 const Formulario = () => {
-  return <h1>Formulario Aquí</h1>;
+  return (
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por Categoría o Ingredientes</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Buscar por Ingredientes"
+          />
+        </div>
+        <div className="col-md-4">
+          <select className="form-control" name="categoria">
+            <option value="">-- Selecciona Categoría --</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Formulario;
