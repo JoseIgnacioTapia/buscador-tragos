8b944176f9177b65d2308bdcf0fa47507ab7767f
JoseIgnacioTapia <josignaciot@gmail.com>
2021-07-27 15:04:04 -0300

    Fixing the display Drink Day Card

--- src/components/CardDrinkDay.js
+++ src/components/CardDrinkDay.js
@@ -5,25 +5,26 @@ import { DrinkDayContext } from '../context/DrinkDayContext';
 import { mostrarIngredientes } from '../helpers/mostrarIngredientes';
 
 const CardDrinkDay = () => {
-  const { drinkDay, lucky } = useContext(DrinkDayContext);
+  const { drinkDay } = useContext(DrinkDayContext);
 
-  const cardContent = lucky ? (
-    <div className="row justify-content-center mt-4">
-      <div className="card col-12 flex-md-row align-items-center">
-        <img
-          className="card-img-left"
-          style={{ width: '50%', height: '250px' }}
-          src={drinkDay.strDrinkThumb}
-          alt="Card image cap"
-        />
-        <div className="card-body">
-          <h5 className="card-title">{drinkDay.strDrink}</h5>
-          <p className="card-text">{drinkDay.strInstructions}</p>
+  const cardContent =
+    Object.keys(drinkDay).length !== 0 ? (
+      <div className="row justify-content-center mt-4">
+        <div className="card col-12 flex-md-row align-items-center">
+          <img
+            className="card-img-left"
+            style={{ width: '50%', height: '250px' }}
+            src={drinkDay.strDrinkThumb}
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{drinkDay.strDrink}</h5>
+            <p className="card-text">{drinkDay.strInstructions}</p>
+          </div>
+          <ul>{mostrarIngredientes(drinkDay)}</ul>
         </div>
-        <ul>{mostrarIngredientes(drinkDay)}</ul>
       </div>
-    </div>
-  ) : null;
+    ) : null;
 
   return cardContent;
 };
